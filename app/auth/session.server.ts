3618f3a67444904157ad818849037f09186d2eb2
Jacob Kostecki <jacob@kostecki.dk>
2025-04-20 13:34:49 +0200

    remove cookie domain

--- app/auth/session.server.ts
+++ app/auth/session.server.ts
@@ -4,7 +4,6 @@ import invariant from "tiny-invariant";
 const maxAge = 60 * 60 * 24; // 24 hours
 
 const SESSION_SECRET = process.env.SESSION_SECRET;
-const COOKIE_DOMAIN = process.env.COOKIE_DOMAIN;
 invariant(SESSION_SECRET, "SESSION_SECRET must be set in .env");
 
 export const { getSession, commitSession, destroySession } =
