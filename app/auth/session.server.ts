e2314d819f69d7e05eff15ac2a0963d7cc79e291
Jacob Kostecki <jacob@kostecki.dk>
2025-04-20 02:23:05 +0200

    set cookie domain

--- app/auth/session.server.ts
+++ app/auth/session.server.ts
@@ -4,7 +4,9 @@ import invariant from "tiny-invariant";
 const maxAge = 60 * 60 * 24; // 24 hours
 
 const SESSION_SECRET = process.env.SESSION_SECRET;
+const COOKIE_DOMAIN = process.env.COOKIE_DOMAIN;
 invariant(SESSION_SECRET, "SESSION_SECRET must be set in .env");
+invariant(COOKIE_DOMAIN, "COOKIE_DOMAIN must be set in .env");
 
 export const { getSession, commitSession, destroySession } =
   createCookieSessionStorage({
@@ -16,5 +18,6 @@ export const { getSession, commitSession, destroySession } =
       httpOnly: true,
       secrets: [SESSION_SECRET],
       secure: process.env.NODE_ENV === "production",
+      domain: COOKIE_DOMAIN,
     },
   });
