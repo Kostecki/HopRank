75f7a35519ae38eae5a1b3470a30e349fce550bd
Jacob Kostecki <jacob@kostecki.dk>
2025-04-21 21:36:06 +0200

    change umami hook

--- app/hooks/umami.ts
+++ app/hooks/umami.ts
@@ -6,21 +6,19 @@ export function useUmamiIdentify(email?: string) {
   const hasIdentified = useRef(false);
 
   useEffect(() => {
-    console.log("useEffect, umami", email, hasIdentified.current);
+    console.log("useEffect running", {
+      email,
+      hasIdentified: hasIdentified.current,
+      pathname: location.pathname,
+    });
+
+    if (hasIdentified.current) return;
     if (!email) return;
     if (location.pathname.startsWith("/auth")) return;
 
-    console.log("useEffect, after", email, hasIdentified.current);
-
-    if (
-      !hasIdentified.current &&
-      typeof window !== "undefined" &&
-      window.umami
-    ) {
-      console.log("umami if", email, hasIdentified.current);
-      window.umami.identify({
-        email: email,
-      });
+    if (typeof window !== "undefined" && window.umami) {
+      console.log("Calling umami.identify", { email });
+      window.umami.identify({ email });
       hasIdentified.current = true;
     }
   }, [email, location.pathname]);
