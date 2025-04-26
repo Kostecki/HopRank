c0ad44e2c4569f2f4a3d852454cc2693699c2f4d
Jacob Kostecki <jacob@kostecki.dk>
2025-04-27 00:31:04 +0200

    add toggle functionality

--- app/components/SelectRatings.tsx
+++ app/components/SelectRatings.tsx
@@ -1,11 +1,35 @@
 import { Box, Chip, Divider, Group, Stack, Switch, Text } from "@mantine/core";
+import { useState } from "react";
+
 import type { SelectRating } from "~/database/schema.types";
 
 type InputProps = {
   ratings: SelectRating[];
 };
 
+const ratingGroups = [
+  {
+    id: "simple",
+    name: "Simpel",
+    active: [1, 2, 3],
+  },
+  {
+    id: "extended",
+    name: "Udvidet",
+    active: [1, 2, 3, 4, 5, 6],
+  },
+];
+
 export default function SelectRatings({ ratings }: InputProps) {
+  const [value, setValue] = useState("simple");
+
+  const setChecked = (ratingId: number) => {
+    const group = ratingGroups.find((group) => group.id === value);
+    if (!group) return false;
+
+    return group.active.includes(ratingId);
+  };
+
   return (
     <>
       <Text fw="bold">Smagnings-kriterier</Text>
@@ -14,17 +38,18 @@ export default function SelectRatings({ ratings }: InputProps) {
       </Text>
 
       <Box my="xs">
-        <Chip.Group>
+        <Chip.Group multiple={false} value={value} onChange={setValue}>
           <Group>
-            <Chip value="1" size="xs" color="slateIndigo">
-              Simpel
-            </Chip>
-            <Chip value="2" size="xs" color="slateIndigo">
-              Udvidet
-            </Chip>
-            <Chip value="3" size="xs" color="slateIndigo">
-              Alle
-            </Chip>
+            {ratingGroups.map((group) => (
+              <Chip
+                key={group.id}
+                value={group.id}
+                size="xs"
+                color="slateIndigo"
+              >
+                {group.name}
+              </Chip>
+            ))}
           </Group>
         </Chip.Group>
       </Box>
@@ -36,7 +61,7 @@ export default function SelectRatings({ ratings }: InputProps) {
           return (
             <Switch
               key={rating.id}
-              defaultChecked={rating.default}
+              checked={setChecked(rating.id)}
               label={rating.name}
               description={rating.description}
               color="slateIndigo"
