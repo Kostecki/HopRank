1e2b0bab723ef9a4f202e03710f043de504f890e
Jacob Kostecki <jacob@kostecki.dk>
2025-04-26 19:37:19 +0200

    debug in prod!

--- app/auth/untappd-strategy.server.ts
+++ app/auth/untappd-strategy.server.ts
@@ -109,12 +109,14 @@ export class UntappdStrategy<User> extends OAuth2Strategy<User> {
     const url = `${TOKEN_ENDPOINT}?${params.toString()}`;
 
     const response = await fetch(url, { method: "GET" });
+    console.log("Untappd token response1", response);
     if (!response.ok) {
       const error = await response.text();
       throw new Error(`Untappd token request failed: ${error}`);
     }
 
     const json = await response.json();
+    console.log("Untappd token response2", json);
 
     if (!json.response?.access_token) {
       throw new Error(`Untappd token response missing access_token`);
