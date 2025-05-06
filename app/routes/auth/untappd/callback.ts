ae0e857f20f4cf481a47bf2427b85669407052aa
Jacob Kostecki <jacob@kostecki.dk>
2025-05-06 19:15:42 +0200

    whatever

--- app/routes/auth/untappd/callback.ts
+++ app/routes/auth/untappd/callback.ts
@@ -8,11 +8,7 @@ export const meta: MetaFunction = () => {
   return [{ title: getPageTitle("Log ind med Untappd") }];
 };
 
-export const loader: LoaderFunction = async ({ request }) => {
-  console.log("Untappd callback loader", request);
-
-  const isDev = import.meta.env.DEV;
-
+export const loader: LoaderFunction = ({ request }) => {
   const url = new URL(request.url);
   const code = url.searchParams.get("code");
 
@@ -20,26 +16,8 @@ export const loader: LoaderFunction = async ({ request }) => {
     return redirect("/auth/login");
   }
 
-  const referer = request.headers.get("referer") || "";
-  const cameFromLocalhost = referer.includes("localhost");
-
-  const isAlreadyOnLocalhost =
-    url.hostname === "localhost" || url.hostname === "127.0.0.1";
-
-  console.log("referer", referer);
-  console.log("cameFromLocalhost", cameFromLocalhost);
-  console.log("isAlreadyOnLocalhost", isAlreadyOnLocalhost);
-
-  if (cameFromLocalhost && !isAlreadyOnLocalhost && isDev) {
-    const localhostURL = new URL("http://localhost:5173/auth/untappd/callback");
-    localhostURL.searchParams.set("code", code);
-
-    console.log("Redirecting to localhost:", localhostURL.toString());
-    return redirect(localhostURL.toString());
-  }
-
   try {
-    return await authenticator.authenticate("Untappd", request);
+    return authenticator.authenticate("Untappd", request);
   } catch (error) {
     console.error("Untappd callback error", error);
     return redirect("/auth/login");
