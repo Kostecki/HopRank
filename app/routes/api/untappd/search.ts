450bdc18afb566cd39827176296bec97d60b31fb
Jacob Kostecki <jacob.kostecki@lego.com>
2025-11-21 12:50:11 +0100

    rename key

--- app/routes/api/untappd/search.ts
+++ app/routes/api/untappd/search.ts
@@ -4,9 +4,9 @@ import type { Route } from "./+types/search";
 import { invariant } from "~/utils/invariant";
 
 const ALGOLIA_APP_ID = import.meta.env.VITE_ALGOLIA_APP_ID;
-const ALGOLIA_API_KEY = import.meta.env.VITE_ALGOLIA_API_KEY;
+const ALGOLIA_API_K = import.meta.env.VITE_ALGOLIA_API_K;
 invariant(ALGOLIA_APP_ID, "VITE_ALGOLIA_APP_ID must be set in .env");
-invariant(ALGOLIA_API_KEY, "VITE_ALGOLIA_API_KEY must be set in .env");
+invariant(ALGOLIA_API_K, "VITE_ALGOLIA_API_K must be set in .env");
 
 export async function loader({ request }: Route.LoaderArgs) {
   const url = new URL(request.url);
@@ -14,7 +14,7 @@ export async function loader({ request }: Route.LoaderArgs) {
 
   if (!searchString) return [];
 
-  const requestUrl = `https://${ALGOLIA_APP_ID}-dsn.algolia.net/1/indexes/beer/query?x-algolia-application-id=${ALGOLIA_APP_ID}&x-algolia-api-key=${ALGOLIA_API_KEY}`;
+  const requestUrl = `https://${ALGOLIA_APP_ID}-dsn.algolia.net/1/indexes/beer/query?x-algolia-application-id=${ALGOLIA_APP_ID}&x-algolia-api-key=${ALGOLIA_API_K}`;
 
   const response = await fetch(requestUrl, {
     method: "POST",
